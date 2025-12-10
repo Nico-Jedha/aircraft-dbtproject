daa29dee111cda14bb3cdb0055f2ad98cd483f31
levasseur.nicolas1@wanadoo.fr <levasseur.nicolas1@wanadoo.fr>
2025-12-10 02:21:29 +0000

    Last commit to save full dbt project

--- models/Stagings/stg_airlines.sql
+++ models/Stagings/stg_airlines.sql
@@ -1,6 +1,6 @@
 SELECT 
-    "Airline_Code" as aircraft_id,
+    "Airline_Code" as airline_code,
     "Airline_Name" as airline_name,
-    "Description" as description_airline
+    "Description" as description
 FROM 
     {{source('aircraft_db', 'AIRLINES')}}
